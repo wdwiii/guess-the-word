d4ebd2b822181647bb63a4795d30faf350ca108a
Willie Whitfield <71739892+wdwiii@users.noreply.github.com>
2021-09-04 23:21:19 -0400

    Add an event listener for the button

--- js/script.js
+++ js/script.js
@@ -16,11 +16,22 @@ let word = 'magnolia'; //Word that needs to be guessed
 //4. For each letter push '●' to dots array
 //5. Join a\dots array items back to a string using the .join("") method.
 const concealWord = word => {
-  let placeholders = [];
+  let placeHolders = [];
   for (let letter of word) {
-    dots.push('●');
+    placeHolders.push('●');
   }
-  wordInProgress.textContent = placeholders.join('');
+  wordInProgress.textContent = placeHolders.join('');
 };
+
+//Function Notes
+//1. When guess button is clicked, prevnt default action (submit) from occuring
+//2. Declare variable that stores the value of the user input
+//3. Clear the value to the input
+btnGuess.addEventListener('click', e => {
+  e.preventDefault();
+  const userGuess = letterInput.value;
+  console.log(userGuess);
+  letterInput.value = '';
+});
 
 concealWord(word);
