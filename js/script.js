e74acd89d613c23faf38bcf36af87a188b92e740
Willie Whitfield <71739892+wdwiii@users.noreply.github.com>
2021-09-04 23:05:58 -0400

    Create function to conceal word
    -Write function to add placeholder for each letter in the word

--- js/script.js
+++ js/script.js
@@ -7,4 +7,20 @@ const wordInProgress = document.querySelector('.word-in-progress');
 const remainingGuesses = document.querySelector('span');
 const message = document.querySelector('.message'); //message will appear when the player guesses a letter
 const btnPlayAgain = document.querySelector('.play-again'); // Button to restart game
-const word = 'magnolia'; //Word that needs to be guessed
+let word = 'magnolia'; //Word that needs to be guessed
+
+//Function Notes
+//1. Create function with a param of 'word'
+//2. Declare dots woth a value of an empty array
+//3. Loop through every letter of the word
+//4. For each letter push '●' to dots array
+//5. Join a\dots array items back to a string using the .join("") method.
+const concealWord = word => {
+  let placeholders = [];
+  for (let letter of word) {
+    dots.push('●');
+  }
+  wordInProgress.textContent = placeholders.join('');
+};
+
+concealWord(word);
